8ec85c7635fba58b5ed51cb97089411adf6bbb02
Sarah Verschuere <sarah.verschuere@ugent.be>
2026-03-09 18:52:54 +0100

    Implementation algorithm 1, 4

--- BitPAl.cpp
+++ BitPAl.cpp
@@ -7,6 +7,12 @@
 
 using namespace std;
 
+// Hard coded match/mismatch score and gap penalty, by consequence the minimum and maximum value for delta_H/V are respectively -3 and 4
+int M = 1;
+int I = -1;
+int G = -3; 
+
+
 void read_fasta_file(const string& file_name, vector<string>& sequences){
     ifstream file(file_name.c_str());
     if (!file){
@@ -48,6 +54,31 @@ void create_match_vectors(vector<uint64_t>& match_vectors, vector<string> sequen
     cout << match_vectors[2];
 }
 
+// deltaVmax_shift = deltaVpos7_shift (algorithm 1 seen during the lecture)
+uint64_t create_deltaVmax_shift(uint64_t deltaHmin, uint64_t match){
+    uint64_t Init_pos_max = 0;
+    uint64_t deltaVmax_shift = 0;
+    Init_pos_max = deltaHmin & match;
+    deltaVmax_shift = ((Init_pos_max + deltaHmin)^deltaHmin)^Init_pos_max;
+    return deltaVmax_shift;
+}
+
+// deltaVhigh(algorithm 2 seen during the lecture)
+uint64_t create_deltaVhigh(uint64_t bit_vectors_delta_H){} //Afwerken
+
+// deltaVlow (algorithm 3 seen during the lecture)
+uint64_t create_deltaVlow(){} //Afwerken
+
+// deltaVmin (algorithm 4 seen during the lecture)
+uint64_t create_deltaVmin(vector<uint64_t> bit_vectors_delta_V){
+    uint64_t deltaHmin_shift = 0;
+    uint64_t all_ones = ~0ULL;
+    uint64_t or_result = 0;
+    for (int i = G + 1; i <= M-G; i++) {
+        or_result |= bit_vectors_delta_V[i];
+    }
+    deltaHmin_shift = all_ones^(or_result);
+}
 
 int main(){
     vector<string> sequences;
@@ -55,4 +86,7 @@ int main(){
 
     vector<uint64_t> match_vectors(4,0); 
     create_match_vectors(match_vectors, sequences);
+
+    vector<uint64_t> bit_vectors_delta_H(M-G-G,0);
+    vector<uint64_t> bit_vectors_delta_V(M-G-G,0);
 }
